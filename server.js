c560563bb3609e1e1f0510916e5750bd85823034
Jef Maldonado <jefmaldonado70@gmail.com>
2024-05-14 19:19:25 -0500

    updated db access and connection

--- server.js
+++ server.js
@@ -4,36 +4,38 @@ const MongoClient = require('mongodb').MongoClient
 const PORT = process.env.PORT ||8000
 require('dotenv').config()
 
-let db,
-    dbConnectionStr = process.env.DB_STRING,
-    dbName = 'recipe-api'
-
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
-    .then(client => {
-        console.log(`Connected to ${dbName} Database`)
-        db = client.db(dbName)
-    })
-
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json())    
 
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'recipe-api'
 
+MongoClient.connect(dbConnectionStr)
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`)
+        db = client.db(dbName)
+        infoCollection = db.collection('recipe-ingredients')
+    
 
-//like an event listener. Network request.
-app.get('/', (request, response) => {
-    response.sendFile(__dirname + '/index.html')
-})
+        //like an event listener. Network request.
+        app.get('/', (request, response) => {
+        response.sendFile(__dirname + '/index.html')
+        })
 
-app.get('/api/:recipe', (request, response) => {
-    const recipeName = request.params.recipe.toLowerCase()
-    if(recipes[recipeName]){
-        response.json(recipes[recipeName])
-    }else{
-        response.json(recipes['unknown'])
-    }
-})
+        app.get('/api/:recipe', (request, response) => {
+        const recipeSubmit = request.params.recipe.toLowerCase()
+            infoCollection.find({recipeName:recipeSubmit}).toArray()
+            .then(results =>{
+                console.log(results)
+                response.json(results[0])
+            })
+            .catch(error => console.err(error))
+        }) 
+    })
+    .catch(error => console.err(error))
 
 app.listen(PORT, () =>{
     console.log(`The server is now running on port ${PORT}.`)
