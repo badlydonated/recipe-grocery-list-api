28b13379f905aa95f66f0238f27b233115e4f842
Jef Maldonado <jefmaldonado70@gmail.com>
2024-05-31 12:01:31 -0500

    fixed code now displays db item names in ejs

--- server.js
+++ server.js
@@ -1,42 +1,38 @@
+
 const express = require('express')
 const app = express()
+const bodyParser = require("body-parser")
 const MongoClient = require('mongodb').MongoClient
+//const mongoose = require('mongoose')
+const ejs = require('ejs')
 const PORT = process.env.PORT ||8000
 require('dotenv').config()
-
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true}))
-app.use(express.json())    
 
 let db,
     dbConnectionStr = process.env.DB_STRING,
     dbName = 'recipe-api'
 
-MongoClient.connect(dbConnectionStr)
+MongoClient.connect(dbConnectionStr, {useUnifiedTopology: true})
     .then(client => {
         console.log(`Connected to ${dbName} Database`)
         db = client.db(dbName)
-        infoCollection = db.collection('recipe-ingredients')
-    
-
-    //like an event listener. Network request.
-    app.get('/', (request, response) => {
-    response.sendFile(__dirname + '/index.html')
     })
 
-    app.get('/api/:recipe', (request, response) => {
-    const recipeSubmit = request.params.recipe.toLowerCase()
-        infoCollection.find({recipeName:recipeSubmit}).toArray()
-        .then(results =>{
-            console.log(results)
-            response.json(results[0])
-        })
-        .catch(error => console.err(error))
-    }) 
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true}))
+app.use(express.json())    
+
+//mongoose.connect('')
+
     
+app.get("/", (request, response) =>{
+    db.collection('recipe-ingredients').find().toArray()
+    .then(data => {
+        response.status(200).render("index", {pageTitle: "home page", list: data})
+    })
 })
-.catch(error => console.err(error))
+        
 
 app.listen(PORT, () =>{
     console.log(`The server is now running on port ${PORT}.`)
